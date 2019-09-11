034f089e41122419e72a8744f16b306db1eee858
plmaclar <pete@95st.com>
2019-09-11 17:00:09 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Are you there Jessalinafina?
+          Jessalinafina was here.
         </p>
         <a
           className="App-link"
